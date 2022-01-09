ce99149256f9118c43b1c0dd8a8757f3f5a356fa
asix360 <alessandro_eddy@hotmail.com>
2022-01-09 14:06:13 -0300

    primeiro commit

---
+++ Inicio/comentario.js
@@ -0,0 +1,4 @@
+//console.log e usando para exibir algo na tela do computador!
+console.log('passo #01');
+console.log('passo #02'); // mais um exemplo de console.log!
+console.log('passo #03');
---
+++ Inicio/dados_4.js
@@ -0,0 +1,5 @@
+const PI = 3.14;
+let raio = 10;
+let circonferencia = PI*(raio*raio);
+
+console.log("A área de circonferencia de raio "+raio+" E : "+ circonferencia+" M2");
